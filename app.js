f4a3fc98ec50778b214d50d41873c1e70ec76f85
s17960 <s17960@pjwstk.edu.pl>
2019-12-09 18:32:57 +0100

    wykonane zadanie 9

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// ---------------------------------------------------------
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
+app.get('/hello', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/form', function(req, res){
+    res.render('form');
+});
+
+app.post('/formdata', function(req, res){
+    res.send(req.body.name + '<br>' + req.body.surname + '<br>' + req.body.city);
+});
+
+app.post('/jsondata', function(req, res){
+    res.json(req.body);
+})
+
+// ---------------------------------------------------------
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
